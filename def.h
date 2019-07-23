caa1f2d2dd7789b5b395a07721606d81edf4296b
Fedor Naumenko <30726965+fnaumenko@users.noreply.github.com>
2019-07-23 11:24:06 +0100

    Delete def.h

--- def.h
+++
@@ -1,6 +0,0 @@
-#define OPT_DESCF_TSHIFT 3	// number of TABs before decsription field in Options::PrintUsage()
-
-#define	_BIOCC
-#define	_FILE_WRITE		// chrom sizes can be written
-//#define _NO_ZLIB
-#define _NO_UNODMAP
